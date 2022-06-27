b3fcbcd912a1a05a4e560b64acb126006c4f3ce2
Vishwas <baghel.vishwas0@gmail.com>
2022-06-28 00:19:44 +0530

    [Vishwas]add: Validating First Name Program Added

---
+++ UserRegistrationProgram/DataValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class DataValidation
+    {
+        public Regex First_Name_Regex = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public void ValidateFirstName()
+        {
+            Console.Write("Enter First Name : ");
+            string firstName = Console.ReadLine();
+
+            if (First_Name_Regex.IsMatch(firstName))
+            {
+                Console.WriteLine("The First Name Is Valid ");
+            }
+            else
+            {
+                Console.WriteLine("The First Name Is Not Valid");
+            }
+        }
+    }
+}
--- UserRegistrationProgram/Program.cs
+++ UserRegistrationProgram/Program.cs
@@ -6,6 +6,26 @@ namespace UserRegistrationProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Program\n========================================");
+            bool end = true;
+            Console.WriteLine("1. Validate First Name\n2. End The Program");
+            while (end)
+            {
+                Console.Write("Choose an option to execute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        DataValidation validating = new DataValidation();
+                        validating.ValidateFirstName();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose correct option");
+                        break;
+                }
+            }
         }
     }
 }
