f3b67e2118ae3b093ff78cbf48f76e79ad810a86
Vishwas <baghel.vishwas0@gmail.com>
2022-06-28 00:45:27 +0530

    [Vishwas]add: Validating Email Address Program Added

--- UserRegistrationProgram/DataValidation.cs
+++ UserRegistrationProgram/DataValidation.cs
@@ -11,6 +11,7 @@ namespace UserRegistrationProgram
     {
         public Regex First_Name_Regex = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}?$");
         public Regex Last_Name_Regex = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public Regex Email_Regex = new Regex("^[A-Za-z]{3,}([.][A-Za-z]{3,})?[@][A-Za-z]{2,}[.][A-Za-z]{2,}([.][A-Za-z]{2})?$");
         public void ValidateFirstName()
         {
             Console.Write("Enter First Name : ");
@@ -37,6 +38,17 @@ namespace UserRegistrationProgram
             else
             {
                 Console.WriteLine("The Last Name Is Not Valid");
+            }
+        }
+        public void ValidateEmail(string email)
+        {
+            if (Email_Regex.IsMatch(email))
+            {
+                Console.WriteLine("Email Is Valid ");
+            }
+            else
+            {
+                Console.WriteLine("Email Is Not Valid ");
             }
         }
     }
--- UserRegistrationProgram/Program.cs
+++ UserRegistrationProgram/Program.cs
@@ -8,7 +8,7 @@ namespace UserRegistrationProgram
             Console.WriteLine("Welcome to the User Registration Program\n========================================");
             DataValidation validating = new DataValidation();
             bool end = true;
-            Console.WriteLine("1. Validate First Name\n2. Validate Last Name\n3. End The Program");
+            Console.WriteLine("1. Validate First Name\n2. Validate Last Name\n3. Validate Email\n4. End The Program");
             while (end)
             {
                 Console.Write("Choose an option to execute : ");
@@ -22,6 +22,12 @@ namespace UserRegistrationProgram
                         validating.ValidateLastName();
                         break;
                     case 3:
+                        validating.ValidateEmail("abc.xyz@bl.co.in");
+                        validating.ValidateEmail("abc111@abc.com");
+                        validating.ValidateEmail("abc@bl.co");
+                        validating.ValidateEmail("abc1232.com");
+                        break;
+                    case 4:
                         end = false;
                         break;
                     default:
