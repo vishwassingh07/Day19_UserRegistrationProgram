035e6dcb936ebd1746f4585e27eb644fb0956860
Vishwas <baghel.vishwas0@gmail.com>
2022-06-27 23:48:35 +0530

    [Vishwas]add: User Registration Program Added

---
+++ UserRegistrationProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UserRegistrationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registration Program\n========================================");
+        }
+    }
+}
